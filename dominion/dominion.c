176f90632f9b6eaf496b97f7890bdaf2c33e0d33
Lee Leighton <leightle@oregonstate.edu>
2016-06-06 06:19:08 -0700

    Re-add original flaw in adventurer

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -659,7 +659,7 @@ void playAdventurer(int currentPlayer, struct gameState *state)
 	int z = 0;		// this is the counter for the temp hand
 	int cardDrawn;
 
-	while (drawntreasure < 3) {
+	while (drawntreasure < 2) {
 		if (state->deckCount[currentPlayer] < 1) {	//if the deck is empty we need to shuffle discard and add to deck
 			shuffle(currentPlayer, state);
 		}
